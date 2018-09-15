9a1b6678c8c14bf6269b384d1726204ef3bdc1ae
evan <ev.lawless@gmail.com>
2018-09-15 11:53:43 -0400

    Preparing to try to add JSX into this project. Fingers crossed.

--- public/main.js
+++ public/main.js
@@ -1,29 +1,2 @@
-
 const h = React.createElement.bind(React);
-
-class DemoOne extends React.Component {
-
-	render() {
-		return h('h2', {},
-			"Demo One"
-		);
-	}
-}
-
-class DemoTwo extends React.Component {
-
-	render() {
-		return h('h2', {},
-			"Demo two"
-		);
-	}
-}
 const container = document.querySelector('#app-view');
-
-function testAppOne() {
-	ReactDOM.render(h(DemoOne), container);
-}
-
-function testAppTwo() {
-	ReactDOM.render(h(DemoTwo), container);
-}
